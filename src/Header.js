fc949a4ec067c66c9a66f31db7b5b00f85a4a6c7
Pavel Vdovenko <reactivecake@gmail.com>
2025-03-11 23:31:34 +0400

    feat(Header): break down button text to 2 lines

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => (
   <header className="header">
     <div className="logo"><h3>trades.ninja</h3></div>
-    <a href="mailto:support@trades.ninja" className="contact-button">Contact us support@trades.ninja</a>
+    <a href="mailto:support@trades.ninja" className="contact-button">Contact us<br/>support@trades.ninja</a>
   </header>
 );
 
